74de70ce446342c9a2ca6fa07a4dce1613d365ad
VlaD77 <ternovoyvladislav@mail.ru>
2016-07-20 02:35:30 +0300

    change costumers/index
    change costumers index
    slider not add

--- frontend/views/customer/index.php
+++ frontend/views/customer/index.php
@@ -1,9 +1,27 @@
 <?php
 /* @var $this yii\web\View */
 ?>
-<h1>customer/index</h1>
+<div class="row page-title text-center">
+            <h2>Замовникам</h2>
+        </div>
+<hr/>
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+<p><span> Замовник</span> - це користувач, что подає питання(проблему) для вирішення.</p>
+
+<ul class="nav"> Замовником можуть виступати:   
+
+    <li class="nav-pills-stacked-example"> 1) </li>
+    <li class="nav-pills-stacked-example"> 2) </li>
+    <li class="nav-pills-stacked-example"> 3) </li>
+    <li class="nav-pills-stacked-example"> 4) </li>
+</ul>    
+
+<p>Якщо ви хочете подати проблему - заповніть наступну форму: </p>
+<div class="text-center">
+<a href="#" class="btn btn-lg btn-danger center">Подати заявку <span class="glyphicon glyphicon-chevron-right"></span></a>
+</div>
+<hr/>
+<div class="row page-title text-center">
+            <h2>Дійсні проблеми</h2>
+        </div>
+<hr/>
